3c978fa6f2469ce5e2bbf69d4aa8c84343fff48f
John Ayeni <johnayeni10.aj@gmail.com>
2019-02-16 01:49:42 +0100

    style: home page design

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Typography, Toolbar, AppBar } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Typography, Toolbar, AppBar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 class Header extends React.PureComponent {
   render() {
     return (
-      <AppBar position='static' color='white'>
+      <AppBar position="static" color="white">
         <Toolbar>
-          <div id='header'>
-          <Link to={"/"}>
-            <Typography variant='h6' color='inherit'>
-              {" "}
-              <span className='text--purple'>The 14th Set</span>{" "}
-            </Typography>
+          <div id="header">
+            <Link to={'/'}>
+              <Typography variant="h5" color="inherit">
+                {' '}
+                <span className="text--purple cursive-text">The 14th Set</span>{' '}
+              </Typography>
             </Link>
           </div>
         </Toolbar>
